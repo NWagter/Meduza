541697095237e90b28e04c9128f0c3d7b90fc65d
Nick_Wagter <wagternick@gmail.com>
2021-12-19 22:10:35 +0000

    [Change] extend the information in the ResourceBrowser

--- Meduza/Source/Platform/General/Editor/EditorResourceBrowser.cpp
+++ Meduza/Source/Platform/General/Editor/EditorResourceBrowser.cpp
@@ -3,7 +3,8 @@
 
 #include "MeduzaIncluder.h"
 
-#include "Platform/General/Resources/Resource.h"
+#include "Platform/General/Resources/MeshBase.h"
+#include "Platform/General/Resources/ShaderBase.h"
 
 Me::Editor::EditorResourceBrowser::EditorResourceBrowser()
 {
@@ -26,38 +27,49 @@ void Me::Editor::EditorResourceBrowser::Draw()
 
 	for (auto r : rLibrary->GetResources())
 	{
-		bool open = ImGui::TreeNodeEx(r.second->GetFileName().c_str(), treeNodeFlags);
+		std::string name;
+
+		switch (r.second->GetType())
+		{
+			case Resources::ResourceType::Audio:
+				name.append("[Audio]   ");
+			break;
+			case Resources::ResourceType::Mesh:
+				name.append("[Mesh]    ");
+			break;
+			case Resources::ResourceType::Script:
+				name.append("[Script]  ");
+				break;
+			case Resources::ResourceType::Shader:
+				name.append("[Shader]  ");
+				break;
+			case Resources::ResourceType::Texture:
+				name.append("[Texture] ");
+				break;
+			default:
+				name.append("[Unknow]  ");
+				break;
+		}
+
+		name.append(r.second->GetFileName());
+
+		bool open = ImGui::TreeNodeEx(name.c_str(), treeNodeFlags);
 		if (open)
 		{
 			ImGui::Text("Resource ID : %u", r.second->GetID());
 			ImGui::Text("Resource Name : %s", r.second->GetFileName().c_str());
-
-			switch (r.second->GetType())
-			{
-			case Resources::ResourceType::Mesh:
-				ImGui::Text("Resource Type : Mesh");
-				break;
-			case Resources::ResourceType::Shader:
-				ImGui::Text("Resource Type : Shader");
-				break;
-			case Resources::ResourceType::Texture:
-				ImGui::Text("Resource Type : Texture");
-				break;
-			case Resources::ResourceType::Audio:
-				ImGui::Text("Resource Type : Audio");
-				break;
-			case Resources::ResourceType::Script:
-				ImGui::Text("Resource Type : Script");
-				break;
-			default:
-				ImGui::Text("Resource Type : Unkown");
-				break;
-			}
-
 			if (!r.second->GetPath().empty())
 			{
 				ImGui::Text("File : %s", r.second->GetPath().c_str());
 			}
+
+			if (r.second->GetType() == Resources::ResourceType::Mesh)
+			{
+				Resources::MeshBase* mesh = static_cast<Resources::MeshBase*>(r.second);
+				ImGui::Text("Vertices : %i", mesh->GetVerticesSize());
+				ImGui::Text("Indices : %i", mesh->GetIndicesSize());
+			}
+			
 			ImGui::TreePop();
 		}
 	}

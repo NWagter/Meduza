46794672056cc1b5fd7ab9601c67dc8317d33f26
Nick_Wagter <wagternick@gmail.com>
2022-01-03 16:38:18 +0000

    [Change] Modify ValueTypes for scripting to have inheritence with a value

--- Meduza/Include/Core/Scripting/ScriptComponentHelper.h
+++ Meduza/Include/Core/Scripting/ScriptComponentHelper.h
@@ -18,6 +18,20 @@ namespace Me
         {
             ValueType m_type;
             std::string m_argumentName;
+        };
+
+        struct ValueInt : public Value
+        {
+            int m_value;
+        };        
+        
+        struct ValueBool : public Value
+        {
+            bool m_value;
+        };        
+        
+        struct ValueString : public Value
+        {
             std::string m_value;
         };
 
@@ -26,12 +40,20 @@ namespace Me
         {
             std::string m_script;
             lua_State* m_luaState;
-            std::vector<Value> m_inputFields;
+            std::vector<Value*> m_inputFields;
 
             Script(std::string const& a_script)
             {
                 m_script = a_script;
                 m_luaState = nullptr;
+            }
+
+            ~Script()
+            {
+                for (auto value : m_inputFields)
+                {
+                    delete value;
+                }
                 m_inputFields.clear();
             }
         };
